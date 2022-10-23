076661637c257229e8f2ce2e03c1fe289213f7bf
Graph1te <egor.anders23@gmail.com>
2022-10-24 01:01:17 +0500

    fixed scroll index.htlml

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,4 +11,24 @@ addEventListener('DOMContentLoaded', () => {
   headerBtn.addEventListener('click', () => {
     menu.classList.toggle('menu--open');
   });
+
+  const carts = document.querySelectorAll('[data-cart]'),
+    decor = document.querySelector('.popup--decor'),
+    closes = document.querySelectorAll('.close img');
+
+  carts.forEach((item) => {
+    item.addEventListener('click', () => {
+      decor.classList.add('popup--open');
+    });
+  });
+
+  closes.forEach((close) => {
+    console.log(close);
+    close.addEventListener('click', (e) => {
+      console.log(123);
+      if (e.target.getAttribute('data-close') == 'decor') {
+        decor.classList.remove('popup--open');
+      }
+    });
+  });
 });
